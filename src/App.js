4014fb80448745060c03e020cb31df759b924619
Joyce Guo <yj.guo@berkeley.edu>
2021-07-29 21:48:31 -0700

    set up server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import React, { useEffect } from "react";
+
+const client = new W3CWebSocket("ws://127.0.0.1:8000");
 
 function App() {
+  useEffect(() => {
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div></div>
+      Testing the app!
     </div>
   );
 }
